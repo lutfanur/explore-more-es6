a377c2c213ce6698e5f27c21b8ef7d652b0f64c3
Lutfaakter <lutfanur20@gmail.com>
2024-02-24 08:18:59 +0000

    added es6 map

---
+++ chain.js
@@ -0,0 +1,41 @@
+// data access
+const data = [{ id : 1, name: 'abul', address: 'kochu khet'}];
+console.log(data[0].address)
+
+const products = {
+    count: 5000,
+    data: [
+        { id: 1, name: 'lenovo laptop', price: 65000}, 
+        {id: 2, name: 'macbook', price: 165000} 
+    ]
+}
+
+//  second product price
+
+// console.log(products.data[1].price)
+
+const user = {
+    id: 5001,
+    name: 'sariful',
+    address: {
+        street: {
+            first: '54/1 uttor side',
+            second: 'pori er goli',
+            third: 'no dorai'
+        },
+        city: 'Dhaka'
+    }
+}
+
+// console.log(user.address.street.second) 
+
+const user2 = {
+    id: 5002,
+    name: 'pori bibir bari',
+    address: {
+        city: 'chittagong',
+        country: 'Bangladesh'
+    }
+}
+console.log(user2.address.street?.second)
+
